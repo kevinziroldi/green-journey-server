daf2ea31264d45145eb553e97813d949f47d00bc
kevinziroldi <k.ziroldi@gmail.com>
2025-05-13 17:22:08 +0200

    removed debug print

--- handlers/reviewhandler.go
+++ handlers/reviewhandler.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"green-journey-server/db"
 	"green-journey-server/externals"
 	"green-journey-server/model"
@@ -555,13 +554,6 @@ func getBestReviews(w http.ResponseWriter, r *http.Request) {
 		log.Println("Error getting best reviews: ", err)
 		http.Error(w, "Error getting best reviews", http.StatusBadRequest)
 		return
-	}
-
-	for i, _ := range bestReviews {
-		fmt.Println(bestReviews[i].Reviews[0].CityIata)
-		fmt.Println(bestReviews[i].AverageGreenSpacesRating)
-		fmt.Println(bestReviews[i].AverageLocalTransportRating)
-		fmt.Println(bestReviews[i].AverageWasteBinsRating)
 	}
 
 	if bestReviews == nil {
